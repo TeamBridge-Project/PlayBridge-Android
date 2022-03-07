62af830235e98fd897bea0015766782ae96eabaa
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-03-07 21:47:07 +0900

    feat #1 : 컬럼 생성

--- PlayBridge/presentation/src/main/java/com/example/presentation/signup/SignUpActivity.kt
+++ PlayBridge/presentation/src/main/java/com/example/presentation/signup/SignUpActivity.kt
@@ -1,4 +1,31 @@
 package com.example.presentation.signup
 
-class SignUpActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.colorspace.Illuminant.B
+
+class SignUpActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContent {
+            Greeting(name = "hi")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Column(
+    ) {
+        Text("회원가입")
+    }
 }
