a744764915778d531b530b04e9d44b0a3653b70e
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-03-07 22:29:14 +0900

    font init

--- PlayBridge/presentation/src/main/java/com/example/presentation/ui/theme/Type.kt
+++ PlayBridge/presentation/src/main/java/com/example/presentation/ui/theme/Type.kt
@@ -2,11 +2,23 @@ package com.example.presentation.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.presentation.R
+
 
 // Set of Material typography styles to start with
+val notosanskr = FontFamily(
+    Font(R.font.notosanskr_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.notosanskr_black, FontWeight.Black, FontStyle.Normal),
+    Font(R.font.notosanskr_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.notosanskr_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.notosanskr_thin, FontWeight.Thin, FontStyle.Normal),
+)
+
 val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
