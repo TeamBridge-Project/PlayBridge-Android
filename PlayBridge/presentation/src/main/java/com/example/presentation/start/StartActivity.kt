7598310adff1ef8136e7b30f4d23fab0a4c6c1eb
GunHyung Ham <gdreagon@naver.com>
2022-03-08 00:58:57 +0900

    style #2 : 시작 페이지작성 중

--- PlayBridge/presentation/src/main/java/com/example/presentation/start/StartActivity.kt
+++ PlayBridge/presentation/src/main/java/com/example/presentation/start/StartActivity.kt
@@ -1,27 +1,31 @@
 package com.example.presentation.start
 
 import android.os.Bundle
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.presentation.R
+import com.example.presentation.ui.theme.Background
 import com.example.presentation.ui.theme.notosanskr
 
 class StartActivity : ComponentActivity() {
@@ -29,6 +33,8 @@ class StartActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val window: Window = this.window
+            window.navigationBarColor = Background.toArgb()
             StartScreen()
         }
     }
@@ -42,21 +48,34 @@ fun StartScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(R.color.background_color))
+            .background(color = Background),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text(
-            text = "Play",
-            fontSize = 75.sp,
-            fontFamily = notosanskr,
-            fontWeight = FontWeight.Bold)
+        Row() {
+            Text(
+                text = "Play",
+                fontSize = 75.sp,
+                fontFamily = notosanskr,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                textAlign = TextAlign.Left,
+                modifier = Modifier.wrapContentHeight(Alignment.Bottom)
+            )
+        }
+        Image(
+            painter =
+                painterResource(
+                    id = R.drawable.ic_baseline_square_foot_24),
+                    contentDescription = ""
+        )
         Text(
             text = "bridge",
             fontSize = 75.sp,
             fontFamily = notosanskr,
             fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(top = 1.dp)
+            //modifier = Modifier.padding(bottom = 200.dp),
+            color = Color.White
         )
-
-
     }
 }
