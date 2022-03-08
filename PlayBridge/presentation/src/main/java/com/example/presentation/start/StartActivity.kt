482c3c6a8f177976b4f83ca0b2ca27c287ce56b0
GunHyung Ham <gdreagon@naver.com>
2022-03-08 23:19:57 +0900

    feat #2 : 시작 화면 제작

--- PlayBridge/presentation/src/main/java/com/example/presentation/start/StartActivity.kt
+++ PlayBridge/presentation/src/main/java/com/example/presentation/start/StartActivity.kt
@@ -1,33 +1,31 @@
 package com.example.presentation.start
 
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.view.WindowCompat
 import com.example.presentation.R
-import com.example.presentation.ui.theme.Background
+import com.example.presentation.ui.theme.BackgroundColor
+import com.example.presentation.ui.theme.ComponentInnerColor
 import com.example.presentation.ui.theme.notosanskr
 
 class StartActivity : ComponentActivity() {
@@ -50,31 +48,63 @@ fun StartScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Background),
-        verticalArrangement = Arrangement.Center,
+            .background(color = BackgroundColor),
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text(
-            text = "Play",
-            fontSize = 75.sp,
-            fontFamily = notosanskr,
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            //textAlign = TextAlign.Left,
-            modifier = Modifier.height(160.dp)
+        Image(
+            painter = painterResource(id = R.drawable.logo_image),
+            contentDescription = "",
+            modifier = Modifier
+                .padding(top = 100.dp)
+                .width(350.dp)
         )
-        Box(modifier = Modifier.fillMaxSize(), Alignment.TopEnd) {
+
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(top = 60.dp)
+                .width(131.dp)
+                .height(50.dp)
+                .shadow(
+                    elevation = 5.dp,
+                    shape = RoundedCornerShape(16.dp),
+                    clip = true
+                ),
+            colors = ButtonDefaults.buttonColors(backgroundColor = ComponentInnerColor),
+            shape = RoundedCornerShape(50)
+        ) {
             Text(
-                text = "bridge",
-                fontSize = 75.sp,
-                fontFamily = notosanskr,
+                text = "로그인",
+                fontSize = 17.sp,
                 fontWeight = FontWeight.Bold,
                 color = Color.White,
-                //textAlign = TextAlign.Right,
-                //modifier = Modifier.height(300.dp)
             )
         }
-        //Spacer(modifier = Modifier.height(50.dp))
 
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(30.dp)
+                .width(131.dp)
+                .height(50.dp)
+                .shadow(
+                    elevation = 5.dp,
+                    shape = RoundedCornerShape(16.dp),
+                    clip = true
+                ),
+            colors = ButtonDefaults.buttonColors(backgroundColor = ComponentInnerColor),
+            shape = RoundedCornerShape(50)
+        ) {
+            Text(
+                text = "회원가입",
+                fontSize = 17.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                modifier = Modifier.padding(bottom = 0.dp)
+            )
+        }
     }
 }
