44410d442d0a7b37c219d4ca287fcabb7eca0f5a
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-03-16 20:55:12 +0900

    refactor #7 : background color 테마에 포함

---
+++ PlayBridge/presentation/src/main/java/com/example/presentation/main/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.presentation.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent{
+
+        }
+    }
+}
+
+@Composable
+fun MainScreen(){
+
+}
+
