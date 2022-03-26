132b23cb78751e6bc94c73289aa9574ddd202632
GunHyung Ham <gdreagon@naver.com>
2022-03-27 00:13:20 +0900

    style #20: 전체 함수 정리 및 PersonalProfileScreen 함수 재정리

--- PlayBridge/presentation/src/main/java/com/example/presentation/personalprofile/PersonalProfileScreen.kt
+++ PlayBridge/presentation/src/main/java/com/example/presentation/personalprofile/PersonalProfileScreen.kt
@@ -1,6 +1,7 @@
 package com.example.presentation.personalprofile
 
-import android.graphics.Color.alpha
+
+import android.provider.ContactsContract
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -8,25 +9,22 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
-import androidx.compose.material.TextFieldDefaults.BackgroundOpacity
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -37,193 +35,42 @@ import com.example.presentation.ui.theme.*
 import com.skydoves.landscapist.CircularReveal
 import com.skydoves.landscapist.glide.GlideImage
 
+
 @Preview(widthDp = 450, heightDp = 850)
 @Composable
-fun preview(){
+fun preview() {
     PersonalProfileScreen(navController = rememberNavController())
 }
 
 @Composable
 fun PersonalProfileScreen(navController: NavController) {
     val isEditing = remember { mutableStateOf(false) }
-    var (text1,setText1) = remember {
-        mutableStateOf("League of Legends - 플레티넘") }
-    var (text2,setText2) = remember {
-        mutableStateOf("기타 게임 사전 협의") }
-    var (text3,setText3) = remember {
-        mutableStateOf("League of Legends - 1시간 5000원 / 1판 3000원") }
-    var (text4,setText4) = remember {
-        mutableStateOf("기타 게임 - 1시간 8000원") }
-    var (selfIntroduction, setSelfIntroduction) = remember {
-        mutableStateOf(
-            "안녕하세요~" +
-                    "\n신규로 들어와서 같이 즐겁게 게임하실분 구하고 있습니다! 게임은 역시 즐겜!"
-        +"\n신규로 들어와서 같이 즐겁게 게임하실분 구하고 있습니다! 게임은 역시 즐겜!"
-                    +"\n신규로 들어와서 같이 즐겁게 게임하실분 구하고 있습니다! 게임은 역시 즐겜!"+"\n신규로 들어와서 같이 즐겁게 게임하실분 구하고 있습니다! 게임은 역시 즐겜!"
-                    +"\n신규로 들어와서 같이 즐겁게 게임하실분 구하고 있습니다! 게임은 역시 즐겜!"
-                    +"\n신규로 들어와서 같이 즐겁게 게임하실분 구하고 있습니다! 게임은 역시 즐겜!"
-
-        )
-    }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = BackgroundColor),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.Start,
+            .background(color = BackgroundColor)
     ) {
-        IconButton(
-            onClick = {navController.popBackStack()},
-            modifier = Modifier.padding(start = 25.dp),
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_baseline_arrow_back_ios_24),
-                contentDescription = "",
-            )
-        }
-
-        CoinQuestionColumn(
-            coinSymbol = stringResource(id = R.string.coin_symbol),
-            textValue = stringResource(id = R.string.coin_quantity),
+        Spacer(modifier = Modifier.height(35.dp))
+        CoinSection(quantity = 3000)
+        ProfilePersonalInformationSection(
+            profileImage = painterResource(id = R.drawable.ic_baseline_account_circle_24),
+            nickname = stringResource(id = R.string.dungledungle),
+            userRating = stringResource(id = R.string.new_rating),
+            gender =  stringResource(id = R.string.male)
         )
-
-        Row(
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            IconButton(
-                onClick = {},
-                modifier = Modifier
-                    .padding(start = 49.dp, end = 24.dp)
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_baseline_account_circle_24),
-                    contentDescription = "",
-                )
-            }
-
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-            ) {
-                Row()
-                {
-                    NicknameColumn(
-                        textValue = stringResource(id = R.string.dungledungle),
-                        fontWeight = FontWeight.Bold
-                    )
-                    NicknameColumn(
-                        textValue = stringResource(id = R.string.nim),
-                        fontWeight = FontWeight.Light
-                    )
-                }
-
-                Row(
-                    modifier = Modifier
-                        .padding(top = 16.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    UserRating(textValue = stringResource(id = R.string.new_rating))
-
-                    Gender(textValue = stringResource(id = R.string.male))
-                }
-            }
-        }
-
         Spacer(modifier = Modifier.height(27.dp))
-
-        Column(
-            modifier = Modifier
-                .width(390.dp)
-                .padding(
-                    start = 50.dp,
-                )
-        ) {
-            TitleText(textValue = "판매자 등록 게임", bottomPaddingValue = 20)
-
-            LazyColumn(
-                modifier = Modifier
-                    .padding(bottom = 25.dp)
-                    .fillMaxWidth(),
-            ) {
-                item {
-                    SellerRegistrationGameItem(
-                        bottomPaddingValue = 17,
-                        isEditing = isEditing,
-                        text = text1,
-                        textChange = setText1
-                    )
-                }
-
-                item {
-                    SellerRegistrationGameItem(
-                        bottomPaddingValue = 0,
-                        isEditing = isEditing,
-                        text = text2,
-                        textChange = setText2
-                    )
-                }
-            }
-        }
-
-        Column(
-            modifier = Modifier
-                .width(390.dp)
-                .padding(
-                    start = 50.dp,
-                )
-        ) {
-            TitleText(textValue = "등록 게임 비용", bottomPaddingValue = 20)
-
-            LazyColumn(
-                modifier = Modifier.padding(bottom = 25.dp)
-            ) {
-                item {
-                    SellerRegistrationGameItem(
-                        bottomPaddingValue = 17,
-                        isEditing = isEditing,
-                        text = text3,
-                        textChange = setText3
-                    )
-                }
-
-                item {
-                    SellerRegistrationGameItem(
-                        bottomPaddingValue = 0,
-                        isEditing = isEditing,
-                        text = text4,
-                        textChange = setText4
-                    )
-                }
-            }
-        }
-
-        Column(
-            modifier = Modifier
-                .padding(
-                    start = 50.dp,
-                )
-        ) {
-            TitleText(textValue = "자기소개", bottomPaddingValue = 0)
-
-            Spacer(modifier = Modifier.height(17.dp))
-
-            SelfIntroductionInputField(
-                isEditing = isEditing,
-                selfIntroduction = selfIntroduction,
-                setSelfIntroduction = setSelfIntroduction
-            )
-        }
-
+        SellerRegistrationGameSection(isEditing = isEditing)
+        RegisteredGameFeeSection(isEditing = isEditing)
+        SelfIntroductionSection(isEditing = isEditing)
         ProfileEditButton(isEditing = isEditing)
     }
+    Backward(navController = navController)
 }
 
 @Composable
-fun CoinQuestionColumn(
-    coinSymbol: String,
-    textValue: String
+fun CoinSection(
+    quantity: Int
 ) {
     Row(
         modifier = Modifier
@@ -235,7 +82,7 @@ fun CoinQuestionColumn(
             onClick = {},
         ) {
             Text(
-                text = coinSymbol,
+                text = "C",
                 color = Color.White,
                 fontSize = 20.sp,
                 fontFamily = notosanskr,
@@ -243,7 +90,7 @@ fun CoinQuestionColumn(
                 modifier = Modifier.padding(end = 5.dp)
             )
             Text(
-                text = textValue,
+                text = quantity.toString(),
                 color = Color.White,
                 fontSize = 20.sp,
                 fontFamily = notosanskr,
@@ -251,7 +98,7 @@ fun CoinQuestionColumn(
             )
         }
 
-        /*GlideImage(
+        GlideImage(
             modifier = Modifier
                 .width(50.dp)
                 .height(50.dp)
@@ -260,33 +107,206 @@ fun CoinQuestionColumn(
             imageModel = ImageBitmap.imageResource(R.drawable.question_mark),
             circularReveal = CircularReveal(duration = 250),
             error = ImageBitmap.imageResource(id = R.drawable.question_mark)
-        )*/
+        )
+    }
+}
+
+@Composable
+fun ProfilePersonalInformationSection(
+    profileImage: Painter,
+    nickname: String,
+    userRating: String,
+    gender: String
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        IconButton(
+            onClick = {},
+            modifier = Modifier
+                .padding(start = 49.dp, end = 24.dp),
+
+            ) {
+            Image(
+                painter = profileImage,
+                contentDescription = "",
+            )
+        }
+
+        Column() {
+            Row()
+            {
+                NicknameColumn(textValue = nickname)
+                Text(
+                    text = stringResource(id = R.string.nim),
+                    color = Color.White,
+                    fontSize = 20.sp,
+                    fontFamily = notosanskr,
+                    fontWeight = FontWeight.Light
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                UserRating(textValue = userRating, ratingColor = NewBadgeColor)
+
+                Gender(textValue = gender, genderColor = MaleBadgeColor)
+            }
+        }
+    }
+}
+
+@Composable
+fun SellerRegistrationGameSection(
+    isEditing: MutableState<Boolean>
+){
+    val sellerRegistrationGameList = remember {
+        mutableStateListOf(
+            "League of Legends - 플레티넘",
+            "기타 게임 사전 협의",
+        )
+    }
+
+    Column(
+        modifier = Modifier
+            .width(390.dp)
+            .padding(
+                start = 50.dp,
+            )
+    ) {
+        TitleText(textValue = "판매자 등록 게임", bottomPaddingValue = 20)
+
+        LazyColumn(
+            modifier = Modifier
+                .padding(bottom = 25.dp)
+                .fillMaxWidth(),
+        ) {
+            item {
+                ListRegistration(
+                    bottomPaddingValue = 17,
+                    isEditing = isEditing,
+                    text = sellerRegistrationGameList[0],
+                    textChange = { sellerRegistrationGameList[0] = it }
+                )
+            }
+
+            item {
+                ListRegistration(
+                    bottomPaddingValue = 0,
+                    isEditing = isEditing,
+                    text = sellerRegistrationGameList[1],
+                    textChange = { sellerRegistrationGameList[1] = it }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun RegisteredGameFeeSection(
+    isEditing: MutableState<Boolean>
+) {
+    val registeredGameFeeList = remember {
+        mutableStateListOf(
+            "리그 오브 레전드 - 1시간 5000원 / 1판 3000원",
+            "기타 게임 - 1시간 8000원",
+        )
+    }
+
+    Column(
+        modifier = Modifier
+            .width(390.dp)
+            .padding(
+                start = 50.dp,
+            )
+    ) {
+        TitleText(textValue = "등록 게임 비용", bottomPaddingValue = 20)
+
+        LazyColumn(
+            modifier = Modifier.padding(bottom = 25.dp)
+        ) {
+            item {
+                ListRegistration(
+                    bottomPaddingValue = 17,
+                    isEditing = isEditing,
+                    text = registeredGameFeeList[0],
+                    textChange = { registeredGameFeeList[0] = it }
+                )
+            }
+
+            item {
+                ListRegistration(
+                    bottomPaddingValue = 0,
+                    isEditing = isEditing,
+                    text = registeredGameFeeList[1],
+                    textChange = { registeredGameFeeList[1] = it }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun SelfIntroductionSection(
+    isEditing: MutableState<Boolean>
+) {
+    var (selfIntroduction, setSelfIntroduction) = remember {
+        mutableStateOf(
+            "안녕하세요~" +
+                    "\n신규로 들어와서 같이 즐겁게 게임하실분 구하고 있습니다! 게임은 역시 즐겜!"
+                    + "\n신규로 들어와서 같이 즐겁게 게임하실분 구하고 있습니다! 게임은 역시 즐겜!"
+                    + "\n신규로 들어와서 같이 즐겁게 게임하실분 구하고 있습니다! 게임은 역시 즐겜!"
+                    + "\n신규로 들어와서 같이 즐겁게 게임하실분 구하고 있습니다! 게임은 역시 즐겜!"
+                    + "\n신규로 들어와서 같이 즐겁게 게임하실분 구하고 있습니다! 게임은 역시 즐겜!"
+                    + "\n신규로 들어와서 같이 즐겁게 게임하실분 구하고 있습니다! 게임은 역시 즐겜!"
+        )
+    }
+    
+    Column(
+        modifier = Modifier
+            .padding(
+                start = 50.dp,
+            )
+    ) {
+        TitleText(textValue = "자기소개", bottomPaddingValue = 0)
+
+        Spacer(modifier = Modifier.height(17.dp))
+
+        SelfIntroductionInputField(
+            isEditing = isEditing,
+            selfIntroduction = selfIntroduction,
+            setSelfIntroduction = setSelfIntroduction
+        )
     }
 }
 
 @Composable
 fun NicknameColumn(
-    textValue: String,
-    fontWeight: FontWeight?
+    textValue: String
 ) {
     Text(
         text = textValue,
         color = Color.White,
         fontSize = 20.sp,
         fontFamily = notosanskr,
-        fontWeight = fontWeight
+        fontWeight = FontWeight.Bold
     )
 }
 
 @Composable
 fun UserRating(
-    textValue: String
+    textValue: String,
+    ratingColor: Color
 ) {
     Button(
         onClick = { },
         enabled = false,
         colors = ButtonDefaults.buttonColors(
-            disabledBackgroundColor = NewBadgeColor,
+            disabledBackgroundColor = ratingColor,
         ),
         modifier = Modifier
             .clip(shape = RoundedCornerShape(10.dp))
@@ -305,12 +325,15 @@ fun UserRating(
 }
 
 @Composable
-fun Gender(textValue: String) {
+fun Gender(
+    textValue: String,
+    genderColor: Color
+) {
     Button(
         onClick = { },
         enabled = false,
         colors = ButtonDefaults.buttonColors(
-            disabledBackgroundColor = MaleBadgeColor,
+            disabledBackgroundColor = genderColor,
         ),
         modifier = Modifier
             .padding(start = 11.28.dp)
@@ -358,10 +381,10 @@ fun DrawDot(
 }
 
 @Composable
-fun SellerRegistrationGameItem(
+fun ListRegistration(
     bottomPaddingValue: Int,
     isEditing: MutableState<Boolean>,
-    text:String,
+    text: String,
     textChange: (String) -> Unit,
 ) {
     val focusRequester = remember { FocusRequester() }
@@ -390,7 +413,6 @@ fun SellerRegistrationGameItem(
             ),
             singleLine = true,
             cursorBrush = SolidColor(Color.White),
-            //overflow = TextOverflow.Clip
         )
 
         Row(
@@ -434,7 +456,8 @@ fun SelfIntroductionInputField(
                 fontSize = 12.sp,
                 color = Color.White,
                 fontFamily = notosanskr,
-                fontWeight = FontWeight.Bold),
+                fontWeight = FontWeight.Bold
+            ),
             colors = TextFieldDefaults.textFieldColors(
                 backgroundColor = SelfIntroduction
             ),
@@ -455,7 +478,7 @@ fun ProfileEditButton(
         horizontalArrangement = Arrangement.Center
     ) {
         Button(
-            onClick = { isEditing.value = !isEditing.value  },
+            onClick = { isEditing.value = !isEditing.value },
 
             colors = ButtonDefaults.buttonColors(
                 backgroundColor = ProfileEditingColor
@@ -465,13 +488,34 @@ fun ProfileEditButton(
                 .width(101.dp)
                 .height(40.dp),
             contentPadding = PaddingValues(0.dp),
-            ) {
+        ) {
             Text(
-                if(!isEditing.value) "편집" else "완료",
+                if (!isEditing.value) "편집" else "완료",
                 fontSize = 15.sp,
                 color = Color.White,
                 fontFamily = notosanskr,
                 fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
+
+@Composable
+fun Backward(
+    navController: NavController
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+    ) {
+        IconButton(
+            onClick = { navController.popBackStack() },
+            modifier = Modifier
+                .padding(start = 35.dp, top = 10.dp),
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_baseline_arrow_back_ios_24),
+                contentDescription = "",
             )
         }
     }
