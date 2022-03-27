443b0e1e81b41bbcf36c41b30d3e76c77e504376
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-03-27 16:03:24 +0900

    feat #23 : 프로필 섹션 추가

--- PlayBridge/presentation/src/main/java/com/example/presentation/registration/AboutProfileScreen.kt
+++ PlayBridge/presentation/src/main/java/com/example/presentation/registration/AboutProfileScreen.kt
@@ -2,11 +2,18 @@ package com.example.presentation.aboutprofile
 
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
 
 import androidx.compose.ui.res.stringResource
 
@@ -20,6 +27,8 @@ import com.example.presentation.ui.util.BackButton
 
 import com.example.presentation.ui.util.RegistrationButton
 import com.example.presentation.ui.util.Title
+import com.skydoves.landscapist.CircularReveal
+import com.skydoves.landscapist.glide.GlideImage
 
 @Composable
 fun AboutProfileScreen(navController : NavController){
@@ -32,6 +41,9 @@ fun AboutProfileScreen(navController : NavController){
         BackButton(navController = navController)
         Spacer(Modifier.height(60.dp))
         Title(stringResource(id = R.string.about_profile_title))
+        Spacer(Modifier.height(60.dp))
+        ProfileSection()
+        Spacer(Modifier.height(60.dp))
     }
     Box(
         modifier = Modifier
@@ -43,5 +55,31 @@ fun AboutProfileScreen(navController : NavController){
             text = "등록",
             navController = navController,
             route = Screens.AboutProfileScreen.route)
+    }
+}
+
+@Composable
+fun ProfileSection(){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 60.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Box(){
+            GlideImage(
+                modifier = Modifier
+                    .width(70.dp)
+                    .height(70.dp)
+                    .clip(CircleShape)
+                    .clickable { },
+                imageModel = ImageBitmap.imageResource(R.drawable.profile_image),
+                circularReveal = CircularReveal(duration = 250),
+                error = ImageBitmap.imageResource(id = R.drawable.profile_image)
+            )
+        }
+
+        Spacer(modifier = Modifier.width(20.dp))
+        Text(text = "프로필 사진", color = Color.White)
     }
 }
