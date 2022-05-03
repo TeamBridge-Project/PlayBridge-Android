c827b626ea8bff5bd42741729a6ad87a67bcfe1d
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-05-03 19:56:49 +0900

    rename #50 : ApiService -> UserService 변경

--- data/src/main/java/com/example/data/repository/UserRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/UserRepositoryImpl.kt
@@ -3,17 +3,16 @@ package com.example.data.repository
 import com.example.data.di.IoDispatcher
 import com.example.data.mapper.ResponseMapper
 import com.example.data.mapper.toData
-import com.example.data.service.ApiService
+import com.example.data.service.UserService
 import com.example.domain.model.LoginModel
 import com.example.domain.model.SignUpModel
 import com.example.domain.repository.UserRepository
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
-    private val apiService: ApiService,
+    private val apiService: UserService,
     @IoDispatcher private val defaultDispatcher: CoroutineDispatcher
 ) : UserRepository {
     override suspend fun signUp(signUpModel: SignUpModel) =
