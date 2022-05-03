0e7d08b116b2bcdba7ae8d5a2a114947dc258f9c
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-05-03 21:34:53 +0900

    feat #50 : domain 모델과 레포지토리 작성

--- domain/src/main/java/com/example/domain/repository/UserRepository.kt
+++ domain/src/main/java/com/example/domain/repository/UserRepository.kt
@@ -2,9 +2,12 @@ package com.example.domain.repository
 
 import com.example.domain.model.LoginModel
 import com.example.domain.model.SignUpModel
+import com.example.domain.model.UserModel
 import com.example.domain.model.UserResponse
+import kotlinx.coroutines.flow.Flow
 
 interface UserRepository {
     suspend fun signUp(signUpModel: SignUpModel): UserResponse
     suspend fun login(loginModel: LoginModel): UserResponse
+    suspend fun getUser(query: Int): Flow<UserModel>
 }
