4d9ad20167220c0eb0faac88bbc090c66165139f
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-05-14 18:19:08 +0900

    refactor #50 : UserModel 변경

--- data/src/main/java/com/example/data/dto/responsebody/UserResponseBody.kt
+++ data/src/main/java/com/example/data/dto/responsebody/UserResponseBody.kt
@@ -1,35 +1,8 @@
 package com.example.data.dto.responsebody
 
-import java.util.Date
+import com.example.domain.model.UserModel
 
 data class UserResponseBody(
-    val status: Boolean,
-    val result: List<User>,
+    val result: List<UserModel>
 )
 
-data class User(
-    val email: String,
-    val password: String,
-    val nickname: String,
-    val profileIcon: String,
-    val gender: String,
-    val birthday: Date,
-    val agreeEmail: Boolean,
-    val credit: Int,
-    val playingGames: List<PlayingGame>,
-    val gameFee: List<GameFee>,
-    val aboutMe: String,
-    val registeredDate: Date
-)
-
-data class PlayingGame(
-    val id: String,
-    val tier: String,
-    val feePerPlay: Int
-)
-
-data class GameFee(
-    val id: String,
-    val feePerHour: Int,
-    val feePerPlay: Int
-)
