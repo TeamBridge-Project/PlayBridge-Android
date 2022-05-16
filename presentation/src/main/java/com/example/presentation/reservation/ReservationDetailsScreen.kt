f3f43eab893f08353e5454f93bd516638310b8ee
GunHyung Ham <gdreagon@naver.com>
2022-05-16 23:57:13 +0900

    feat #56: private 접근 제한자 추가 및 컴포저블 함수 단일화

--- presentation/src/main/java/com/example/presentation/reservation/ReservationDetailsScreen.kt
+++ presentation/src/main/java/com/example/presentation/reservation/ReservationDetailsScreen.kt
@@ -40,6 +40,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.presentation.R
+import com.example.presentation.reservation.components.ReservationContent
 import com.example.presentation.ui.theme.BackgroundColor
 import com.example.presentation.ui.theme.ReservationAcceptButtonColor
 import com.example.presentation.ui.theme.ReservationRefuseButtonColor
@@ -47,211 +48,177 @@ import com.example.presentation.ui.theme.SelfIntroduction
 import com.example.presentation.ui.theme.notosanskr
 
 @Composable
-fun ReservationDetailsScreen(navController: NavController) {
+private fun ReservationDetailsScreen(navController: NavController) {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(color = BackgroundColor)
     ) {
-        TopSection(navController)
+        Row(
+            modifier = Modifier
+                .padding(start = 35.dp, top = 10.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(
+                onClick = { navController.popBackStack() },
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_baseline_arrow_back_ios_24),
+                    contentDescription = "",
+                )
+            }
+
+            Text(
+                text = stringResource(id = R.string.reservation_details),
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                fontFamily = notosanskr
+            )
+        }
+
         Spacer(modifier = Modifier.height(50.dp))
+
         Column(
             modifier = Modifier.padding(start = 70.dp)
         ) {
-            UserProfileSection()
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.nyong),
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(99.dp)
+                        .clip(CircleShape)
+                )
+
+                Spacer(modifier = Modifier.width(19.dp))
+
+                Text(
+                    text = buildAnnotatedString {
+                        append("둥글둥글")
+                        withStyle(SpanStyle(fontSize = 15.sp, fontWeight = FontWeight.Medium)) {
+                            append("님")
+                        }
+                    },
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    fontFamily = notosanskr
+                )
+            }
+
             Spacer(modifier = Modifier.height(40.dp))
+
             ReservationContent(
                 textValue = stringResource(id = R.string.reservation_date),
                 contentText = "2022.4.9 토요일"
             )
+
             Spacer(modifier = Modifier.height(29.dp))
+
             ReservationContent(
                 textValue = stringResource(id = R.string.hours_of_use),
                 contentText = "14 : 00 - 17 : 00"
             )
+
             Spacer(modifier = Modifier.height(29.dp))
+
             ReservationContent(
                 textValue = stringResource(id = R.string.reservation_fee),
                 contentText = "300"
             )
+
             Spacer(modifier = Modifier.height(50.dp))
-            MemoSection(text = stringResource(id = R.string.memo))
+
+            Text(
+                text = stringResource(id = R.string.memo),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                fontFamily = notosanskr
+            )
+
+            Spacer(modifier = Modifier.height(17.dp))
+
+            Row(
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState()),
+            ) {
+                TextField(
+                    readOnly = true,
+                    modifier = Modifier
+                        .width(300.dp)
+                        .height(170.dp)
+                        .clip(shape = RoundedCornerShape(30.dp)),
+                    textStyle = TextStyle(
+                        fontSize = 12.sp,
+                        color = Color.White,
+                        fontFamily = notosanskr,
+                        fontWeight = FontWeight.Bold
+                    ),
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = SelfIntroduction
+                    ),
+                    value = "안녕하세요 ~ \n" +
+                            "\n" +
+                            "들어오시기 전에 채팅으로 게임 닉네임만\n" +
+                            "알려주세요!",
+                    onValueChange = { } ,
+                )
+            }
+
             Spacer(modifier = Modifier.height(60.dp))
         }
-        ButtonLine()
-    }
-}
 
-@Composable
-fun ReservationContent(
-    textValue: String,
-    contentText: String
-) {
-    Row(
-        modifier = Modifier.width(300.dp),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            text = textValue,
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            fontFamily = notosanskr
-        )
-        Text(
-            text = buildAnnotatedString {
-                                        append(contentText)
-                if(textValue == stringResource(id = R.string.reservation_fee)) {
-                    append(" C")
-                }
-            },
-            fontSize = 15.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            fontFamily = notosanskr
-        )
-    }
-}
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Button(
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = ReservationAcceptButtonColor
+                ),
+                modifier = Modifier
+                    .clip(shape = RoundedCornerShape(30.dp))
+                    .width(101.dp)
+                    .height(40.dp),
+                contentPadding = PaddingValues(0.dp),
+            ) {
+                Text(
+                    text = stringResource(id = R.string.reservation_accept),
+                    fontSize = 12.sp,
+                    color = Color.White,
+                    fontFamily = notosanskr,
+                    fontWeight = FontWeight.Bold
+                )
+            }
 
-@Composable
-fun ButtonLine() {
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.Center
-    ) {
-        Button(
-            onClick = { },
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = ReservationAcceptButtonColor
-            ),
-            modifier = Modifier
-                .clip(shape = RoundedCornerShape(30.dp))
-                .width(101.dp)
-                .height(40.dp),
-            contentPadding = PaddingValues(0.dp),
-        ) {
-            Text(
-                text = stringResource(id = R.string.reservation_accept),
-                fontSize = 12.sp,
-                color = Color.White,
-                fontFamily = notosanskr,
-                fontWeight = FontWeight.Bold
-            )
-        }
-        Spacer(modifier = Modifier.width(23.dp))
-        Button(
-            onClick = { },
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = ReservationRefuseButtonColor
-            ),
-            modifier = Modifier
-                .clip(shape = RoundedCornerShape(30.dp))
-                .width(101.dp)
-                .height(40.dp),
-            contentPadding = PaddingValues(0.dp),
-        ) {
-            Text(
-                text = stringResource(id = R.string.reservation_refuse),
-                fontSize = 12.sp,
-                color = Color.White,
-                fontFamily = notosanskr,
-                fontWeight = FontWeight.Bold
-            )
+            Spacer(modifier = Modifier.width(23.dp))
+
+            Button(
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = ReservationRefuseButtonColor
+                ),
+                modifier = Modifier
+                    .clip(shape = RoundedCornerShape(30.dp))
+                    .width(101.dp)
+                    .height(40.dp),
+                contentPadding = PaddingValues(0.dp),
+            ) {
+                Text(
+                    text = stringResource(id = R.string.reservation_refuse),
+                    fontSize = 12.sp,
+                    color = Color.White,
+                    fontFamily = notosanskr,
+                    fontWeight = FontWeight.Bold
+                )
+            }
         }
     }
 }
 
-@Composable
-fun MemoSection(text: String) {
-    Text(
-        text = text,
-        fontSize = 20.sp,
-        fontWeight = FontWeight.Bold,
-        color = Color.White,
-        fontFamily = notosanskr
-    )
-    Spacer(modifier = Modifier.height(17.dp))
-    Row(
-        modifier = Modifier
-            .verticalScroll(rememberScrollState()),
-    ) {
-        TextField(
-            readOnly = true,
-            modifier = Modifier
-                .width(300.dp)
-                .height(170.dp)
-                .clip(shape = RoundedCornerShape(30.dp)),
-            textStyle = TextStyle(
-                fontSize = 12.sp,
-                color = Color.White,
-                fontFamily = notosanskr,
-                fontWeight = FontWeight.Bold
-            ),
-            colors = TextFieldDefaults.textFieldColors(
-                backgroundColor = SelfIntroduction
-            ),
-            value = "안녕하세요 ~ \n" +
-                    "\n" +
-                    "들어오시기 전에 채팅으로 게임 닉네임만\n" +
-                    "알려주세요!",
-            onValueChange = { } ,
-        )
-    }
-}
-
-@Composable
-fun UserProfileSection() {
-    Row(
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Image(
-            painter = painterResource(R.drawable.nyong),
-            contentDescription = "",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .size(99.dp)
-                .clip(CircleShape)
-        )
-        Spacer(modifier = Modifier.width(19.dp))
-        Text(
-            text = buildAnnotatedString {
-                append("둥글둥글")
-                withStyle(SpanStyle(fontSize = 15.sp, fontWeight = FontWeight.Medium)) {
-                    append("님")
-                }
-            },
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            fontFamily = notosanskr
-        )
-    }
-}
-
-@Composable
-fun TopSection(
-    navController: NavController
-) {
-    Row(
-        modifier = Modifier
-            .padding(start = 35.dp, top = 10.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        IconButton(
-            onClick = { navController.popBackStack() },
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_baseline_arrow_back_ios_24),
-                contentDescription = "",
-            )
-        }
-        Text(
-            text = stringResource(id = R.string.reservation_details),
-            fontSize = 15.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            fontFamily = notosanskr
-        )
-    }
-}
