10c8586e4d6f08a594ee28cedd3a37d6b52433e0
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-05-16 22:15:43 +0900

    !hotpix #63 : 잘못 머지된 코드 제거

--- presentation/src/main/java/com/example/presentation/start/StartViewModel.kt
+++ presentation/src/main/java/com/example/presentation/start/StartViewModel.kt
@@ -4,16 +4,13 @@ import android.app.Activity
 import android.content.Intent
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.domain.base.processMore
 import com.example.domain.model.LoginModel
 import com.example.domain.usecase.LoginUseCase
 import com.example.local.datastore.DataStoreManager
 import com.example.presentation.main.MainActivity
 import com.example.presentation.signup.SignUpActivity
 import com.example.presentation.util.sha256
-import com.skydoves.sandwich.onError
 import com.skydoves.sandwich.onFailure
-import com.skydoves.sandwich.onSuccess
 import com.skydoves.sandwich.suspendOnSuccess
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -36,7 +33,7 @@ class StartViewModel @Inject constructor(
     ) {
         _uiState.value = StartState.Loading
         viewModelScope.launch {
-            loginUseCase(LoginModel(email, password.sha256())).suspendOnSuccess{
+            loginUseCase(LoginModel(email, password.sha256())).suspendOnSuccess {
                 val accessToken = headers["X-Access-Token"]!!
                 val refreshToken = headers["X-Refresh-Token"]!!
                 dataStore.setAccessToken(accessToken)
