39246dc74b6ac6a9eb27a83816f0b083d91a3994
GunHyung Ham <gdreagon@naver.com>
2022-05-19 21:05:20 +0900

    feat #58: StartState 실드 클래스에 로그인 실패시 발생되는 Failed 오브젝트 추가

--- presentation/src/main/java/com/example/presentation/start/StartState.kt
+++ presentation/src/main/java/com/example/presentation/start/StartState.kt
@@ -3,5 +3,6 @@ package com.example.presentation.start
 sealed class StartState {
     object LoginNeeded : StartState()
     object Success : StartState()
+    object Failed : StartState()
     object Loading : StartState()
 }
