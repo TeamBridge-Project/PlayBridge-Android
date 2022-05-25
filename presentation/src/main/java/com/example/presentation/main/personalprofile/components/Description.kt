e1adfec02bd34c062ee7258ec76406267a4e6f74
GunHyung Ham <gdreagon@naver.com>
2022-05-26 00:20:17 +0900

    refactor #93: Description 리팩토링

--- presentation/src/main/java/com/example/presentation/main/personalprofile/components/Description.kt
+++ presentation/src/main/java/com/example/presentation/main/personalprofile/components/Description.kt
@@ -1,19 +1,15 @@
 package com.example.presentation.main.personalprofile.components
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -21,18 +17,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.presentation.R
 import com.example.presentation.main.common.DrawDot
+import com.example.presentation.main.personalprofile.common.PencilIcon
 import com.example.presentation.ui.theme.PointColor
 import com.example.presentation.ui.theme.notosanskr
 
@@ -46,13 +40,19 @@ fun Description(
         modifier = Modifier
             .fillMaxWidth()
     ) {
-        Text(
-            text = sectionName,
-            color = Color.White,
-            fontSize = 20.sp,
-            fontFamily = notosanskr,
-            fontWeight = FontWeight.Bold,
-        )
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = sectionName,
+                color = Color.White,
+                fontSize = 20.sp,
+                fontFamily = notosanskr,
+                fontWeight = FontWeight.Bold,
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            PencilIcon(isEditing = isEditing)
+        }
 
         Spacer(modifier = Modifier.height(20.dp))
 
@@ -99,24 +99,6 @@ fun Element(
             singleLine = true,
             cursorBrush = SolidColor(Color.White),
         )
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.End
-        ) {
-            IconButton(
-                onClick = { focusRequester.requestFocus() },
-                enabled = isEditing.value,
-                modifier = Modifier
-                    .then(Modifier.size(24.dp))
-                    .alpha(if (!isEditing.value) 0f else 1f),
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_baseline_help_24),
-                    contentDescription = "",
-                )
-            }
-        }
     }
 
     Spacer(modifier = Modifier.height(17.dp))
