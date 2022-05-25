5f90d4b9bbd44245d6a20220f0a1a042a15d5f1e
GunHyung Ham <gdreagon@naver.com>
2022-05-26 00:19:49 +0900

    refactor #93: CoinSection 리팩토링

--- presentation/src/main/java/com/example/presentation/main/personalprofile/components/CoinSection.kt
+++ presentation/src/main/java/com/example/presentation/main/personalprofile/components/CoinSection.kt
@@ -1,69 +1,69 @@
 package com.example.presentation.main.personalprofile.components
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
-import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import com.example.presentation.R
+import com.example.presentation.ui.common.noRippleClickable
+import com.example.presentation.ui.theme.BackgroundColor
 import com.example.presentation.ui.theme.notosanskr
 import com.skydoves.landscapist.CircularReveal
 import com.skydoves.landscapist.coil.CoilImage
 
 @Composable
-fun CoinSection() {
+fun CoinSection(
+    coin: String,
+    navController: NavController
+) {
     Row(
         modifier = Modifier
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .background(BackgroundColor),
         horizontalArrangement = Arrangement.End,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        TextButton(
-            onClick = { },
-        ) {
-            Text(
-                text = stringResource(id = R.string.coin_mark),
-                color = Color.White,
-                fontSize = 20.sp,
-                fontFamily = notosanskr,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(end = 5.dp)
-            )
+        Text(
+            text = buildAnnotatedString {
+                append(stringResource(id = R.string.coin_mark))
+                append(" ")
+                append(coin)
+            },
+            color = Color.White,
+            fontSize = 20.sp,
+            fontFamily = notosanskr,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .noRippleClickable { }
+        )
 
-            Text(
-                text = "3000",
-                color = Color.White,
-                fontSize = 20.sp,
-                fontFamily = notosanskr,
-                fontWeight = FontWeight.Bold
-            )
-        }
+        Spacer(modifier = Modifier.width(10.dp))
 
         CoilImage(
             modifier = Modifier
-                .width(50.dp)
-                .height(50.dp)
+                .size(23.dp)
                 .clip(CircleShape)
-                .clickable(enabled = true, onClick = {}),
-            imageModel = ImageBitmap.imageResource(R.drawable.question_mark),
+                .noRippleClickable { },
+            imageModel = painterResource(id = R.drawable.question_icon),
             circularReveal = CircularReveal(duration = 250),
-            error = ImageBitmap.imageResource(id = R.drawable.question_mark)
+            error = painterResource(id = R.drawable.question_icon),
         )
     }
 }
